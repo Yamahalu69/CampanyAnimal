c0fef93ecf4ac5abaa6be551b4a095649ac8a1ee
5F\ktc <>
2025-06-20 16:13:16 +0900

    GameClearScene、GameOverSceneを追加しました。 GameManagerを変更しました。

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,21 +1,44 @@
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
+    public static GameManager instance;
+
     public TaskManager taskManager;
+
+    void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    void Start()
+    {
+        GameStart();
+    }
 
     void GameStart()
     {
-
+        taskManager.Init();
     }
 
-    void GameClear()
+    public void GameClear()
     {
-
+        Debug.Log("GameClear");
+        SceneManager.LoadScene("GameClearScene");
     }
 
-    void GameOver()
+    public void GameOver()
     {
-
+        Debug.Log("GameOver");
+        SceneManager.LoadScene("GameOverScene");
     }
 }
