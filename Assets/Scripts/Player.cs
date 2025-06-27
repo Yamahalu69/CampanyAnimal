eaebc3db9ac32f790037541c7197dca0ba12b4a7
5F\ktc <>
2025-06-27 19:25:27 +0900

    スクリプト内のバグ修正

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -153,21 +153,13 @@ public class Player : MonoBehaviour
         //タスク開始
         if (Input.GetKeyDown(KeyCode.Return) && dsencer == true)
         {
-<<<<<<< HEAD
-            Debug.Log("bbb");
             displaytask.SetActive(true);
-=======
->>>>>>> 5d4ed6a6199e1d3bb3380aa60ffeeb38978cc7b9
             pl = false;
-            displaytask.SetActive(true); 
         }
-<<<<<<< HEAD
-        //タスク終了
-        else if (Input.GetKeyDown(KeyCode.Space) && dsencer == true) 
-=======
+
         //タスク中断
         if (Input.GetKeyDown(KeyCode.Space) && dsencer == true) 
->>>>>>> 5d4ed6a6199e1d3bb3380aa60ffeeb38978cc7b9
+
         {
             displaytask.SetActive(false);
             pl = true;
