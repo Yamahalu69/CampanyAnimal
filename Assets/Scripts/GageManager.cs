f71d542b2612c8812832c314bad5c2e9cd380a38
5F\ktc <>
2025-07-11 19:20:21 +0900

    ゲージの連動

--- Assets/Scripts/GageManager.cs
+++ Assets/Scripts/GageManager.cs
@@ -1,32 +1,50 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 using UnityEngine.UI;
 
 public class GageManager : MonoBehaviour
 {
-    public Slider slider;　//スライダーの参照
-    public float duration = 60f; //ゴールまでの時間(秒)
+    public GameObject targetObject;  // 表示を監視するオブジェクト
+    public GameObject sliderObject;  // スライダーUIのGameObject
+    public Slider slider;            // スライダーコンポーネント
+    public float duration = 60f;      // ゲージが満タンになる時間
 
-    private float timeEleapsed = 0f; //経過時間
-
-    void Start()
-    { 
-
-    }
-
+    private float timeElapsed = 0f;
+    private bool isRunning = false;
     void Update()
     {
-        //時間経過するごとにスライダーの値を更新
-        if (timeEleapsed < duration)
+        // targetObjectがアクティブならスライダーを表示してゲージを進める
+        if (targetObject != null && targetObject.activeInHierarchy)
         {
-            timeEleapsed += Time.deltaTime; //経過時間を更新
-            slider.value = Mathf.Lerp
-                (0f,1f,timeEleapsed / duration); //スライダーの値を更新(0～1に補間)
+            if (!isRunning)
+            {
+                // 表示された瞬間にリセット＆表示
+                timeElapsed = 0f;
+                slider.value = 0f;
+                sliderObject.SetActive(true);
+                isRunning = true;
+            }
+
+            // ゲージを進める
+            if (timeElapsed < duration)
+            {
+                timeElapsed += Time.deltaTime;
+                slider.value = Mathf.Lerp(0f, 1f, timeElapsed / duration);
+            }
+            else
+            {
+                isRunning = false;
+                GameManager.instance.OverWorkGameOver();
+            }
         }
         else
         {
-            GameManager.instance.OverWorkGameOver();
+            // targetObjectが非表示ならスライダーも非表示にして停止
+            if (isRunning)
+            {
+                sliderObject.SetActive(false);
+                isRunning = false;
+                slider.value = 0f;
+            }
         }
     }
 }
