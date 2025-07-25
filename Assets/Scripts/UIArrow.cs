cbdc30df7ba4a032a4617fb8e6daac606cb3e29e
5F\ktc <>
2025-07-25 18:52:31 +0900

    矢印完成

--- Assets/Scripts/UIArrow.cs
+++ Assets/Scripts/UIArrow.cs
@@ -4,8 +4,7 @@ using UnityEngine.UI;
 [RequireComponent(typeof(RectTransform))]
 public class UIArrow : MonoBehaviour
 {
-    [Header("タスク位置案内")]
-    [SerializeField] private Transform taskpos =default;
+    [SerializeField, Header("タスク位置案内")] private Transform taskpos = default;
     [SerializeField] private Camera maincamera;
     [SerializeField] private Image arrow = default;
     private RectTransform rectTransform;
@@ -18,13 +17,10 @@ public class UIArrow : MonoBehaviour
     void Start()
     {
         var list = GameManager.instance.taskManager.TaskGOs();
-        maincamera=Camera.main;
+        maincamera = Camera.main;
     }
 
-    private void Update()
-    {
-       
-    }
+    
 
     // Update is called once per frame
     void LateUpdate()
@@ -32,42 +28,40 @@ public class UIArrow : MonoBehaviour
         if (!taskpos) return;
 
         float canvasscale = transform.root.localScale.z;
-        var center = 0.5f * new Vector3(Screen.width, Screen.height)*0.5f;
+        var center =new Vector3(Screen.width, Screen.height) * 0.5f;
 
-        var pos = maincamera.WorldToScreenPoint(taskpos.position)-center;
+        var pos = maincamera.WorldToScreenPoint(taskpos.position) - center;
 
-        if(pos.z<0f)
+        if (pos.z < 0f)
         {
             pos.x = -pos.x;
             pos.y = -pos.y;
 
+            if (Mathf.Approximately(pos.y, 0f))
+            {
+                pos.y = -center.y;
+            }
         }
 
-        var halfsize = 0.5f*canvasscale*rectTransform.sizeDelta;
-        float d  = Mathf.Max(
-            Mathf.Abs(pos.x/(center.x-halfsize.x)),
-            Mathf.Abs(pos.y/(center.y-halfsize.y))
+        var halfsize = 0.5f * canvasscale * rectTransform.sizeDelta;
+        float d = Mathf.Max(
+            Mathf.Abs(pos.x / (center.x - halfsize.x)),
+            Mathf.Abs(pos.y / (center.y - halfsize.y))
         );
 
-        bool isoffscreen = (pos.x < 0f || d > 1f);
-        if(isoffscreen)
+        bool isoffscreen = d > 1f;
+        if (isoffscreen)
         {
             pos.x /= d;
             pos.y /= d;
         }
 
-        rectTransform.anchoredPosition = pos/canvasscale;
-        if(isoffscreen)
-        {
-            Vector2 direction = pos.normalized;
-            float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
-            arrow.rectTransform.eulerAngles = new Vector3(0f, 0f, angle);
-        }
-        else
-        {
-            arrow.rectTransform.eulerAngles = Vector3.zero;
-        }
+        rectTransform.anchoredPosition = pos / canvasscale;
 
-         arrow.enabled = true;
+        arrow.enabled = true;
+        arrow.rectTransform.eulerAngles=isoffscreen
+            ?new Vector3(0f,0f,Mathf.Atan2(pos.y,pos.x)* Mathf.Rad2Deg)
+            :Vector3.zero;
+
     }
 }
