1dc999e318e13992172a00dcd1cb6151a44fa927
5F\ktc <>
2025-08-04 16:17:30 +0900

    効果音再生用スクリプト

--- Assets/Scripts/AudioManager.cs
+++ Assets/Scripts/AudioManager.cs
@@ -3,6 +3,13 @@ using UnityEngine;
 public class AudioManager : MonoBehaviour
 {
     public static AudioManager instance;
+
+    [SerializeField]
+    private AudioSource bgmSource, seSource;
+
+    [SerializeField]
+    private AudioClip title, playing, gameover, gameclear, uiclick, warning;
+
     void Awake()
     {
         if (instance == null)
@@ -15,5 +22,42 @@ public class AudioManager : MonoBehaviour
         }
     }
 
+    public void TitleBGM()
+    {
+        bgmSource.clip = title;
+        bgmSource.Play();
+    }
 
+    public void PlayingBGM()
+    {
+        bgmSource.clip = playing;
+        bgmSource.Play();
+    }
+
+    public void GameoverBGM()
+    {
+        bgmSource.clip = gameover;
+        bgmSource.Play();
+    }
+
+    public void GameclearBGM()
+    {
+        bgmSource.clip = gameclear;
+        bgmSource.Play();
+    }
+
+    public void WarningSE()
+    {
+        seSource.PlayOneShot(warning);
+    }
+
+    public void UIClick()
+    {
+        seSource.PlayOneShot(uiclick);
+    }
+
+    void Start()
+    {
+        TitleBGM();
+    }
 }
