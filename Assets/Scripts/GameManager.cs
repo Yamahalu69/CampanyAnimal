cb696bbc400bc950ce9247b40a2292876a34bf2b
5F\ktc <>
2025-08-22 14:55:40 +0900

    TimeEventのScriptableObject化

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -5,81 +5,6 @@ using System.Linq;
 
 public class GameManager : MonoBehaviour
 {
-    private class TimeEvent
-    {
-        public enum EventList
-        {
-            CustomerRaid,
-            AddStockTask,
-            AddDisplayTask,
-            AddCleanTask,
-            StopRandomSpawn
-        }
-        /// <summary>
-        /// 
-        /// </summary>
-        /// <param name="triggerT">イベント発生時間(経過時間)</param>
-        /// <param name="event">発生させるイベント</param>
-        public TimeEvent(float triggerT, EventList @event)
-        {
-            triggerTime = triggerT;
-            triggered = false;
-            actionEvent = @event;
-        }
-        public float triggerTime;
-        public bool triggered;
-        public EventList actionEvent;
-
-        public void ActionEvent()
-        {
-            triggered = true;
-            switch (actionEvent)
-            {
-                case EventList.CustomerRaid:
-                    CustomerRaid();
-                    break;
-                case EventList.AddStockTask:
-                    AddStockTask();
-                    break;
-                case EventList.AddDisplayTask:
-                    AddDisplayTask();
-                    break;
-                case EventList.AddCleanTask:
-                    AddCleanTask();
-                    break;
-                case EventList.StopRandomSpawn:
-                    StopRandomSpawn();
-                    break;
-            }
-        }
-
-        public void CustomerRaid()
-        {
-            GameManager.instance.taskManager.AddTask(Task.register);
-            //NPC
-        }
-
-        public void AddStockTask()
-        {
-            GameManager.instance.taskManager.AddTask(Task.stocking);
-        }
-
-        public void AddDisplayTask()
-        {
-            GameManager.instance.taskManager.AddTask(Task.display);
-        }
-
-        public void AddCleanTask()
-        {
-            GameManager.instance.taskManager.AddTask(Task.cleaning);
-        }
-
-        public void StopRandomSpawn()
-        {
-            GameManager.instance.spawnTask = false;
-        }
-    }
-
     public static GameManager instance;
 
     public TaskManager taskManager;
@@ -91,13 +16,8 @@ public class GameManager : MonoBehaviour
     public float randomSpawnMin = 0;
     public float randomSpawnMax = 10;
 
-    private List<TimeEvent> events = new List<TimeEvent>()
-    {
-        new TimeEvent(10, TimeEvent.EventList.CustomerRaid),
-        new TimeEvent(20, TimeEvent.EventList.AddDisplayTask),
-        new TimeEvent(30, TimeEvent.EventList.AddStockTask),
-        new TimeEvent(50, TimeEvent.EventList.StopRandomSpawn),
-    };
+    [SerializeField]
+    private List<TimeEvent> events = new List<TimeEvent>();
 
     void Awake()
     {
---
+++ Assets/Scripts/TimeEvent.cs
@@ -0,0 +1,78 @@
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "TimeEvent", menuName = "Scriptable Objects/TimeEvent")]
+public class TimeEvent : ScriptableObject
+{
+    public enum EventList
+    {
+        CustomerRaid,
+        AddStockTask,
+        AddDisplayTask,
+        AddCleanTask,
+        StopRandomSpawn
+    }
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <param name="triggerT">イベント発生時間(経過時間)</param>
+    /// <param name="event">発生させるイベント</param>
+    public TimeEvent(float triggerT, EventList @event)
+    {
+        triggerTime = triggerT;
+        triggered = false;
+        actionEvent = @event;
+    }
+    public float triggerTime;
+    [HideInInspector]
+    public bool triggered;
+    public EventList actionEvent;
+
+    public void ActionEvent()
+    {
+        triggered = true;
+        switch (actionEvent)
+        {
+            case EventList.CustomerRaid:
+                CustomerRaid();
+                break;
+            case EventList.AddStockTask:
+                AddStockTask();
+                break;
+            case EventList.AddDisplayTask:
+                AddDisplayTask();
+                break;
+            case EventList.AddCleanTask:
+                AddCleanTask();
+                break;
+            case EventList.StopRandomSpawn:
+                StopRandomSpawn();
+                break;
+        }
+    }
+
+    public void CustomerRaid()
+    {
+        GameManager.instance.taskManager.AddTask(Task.register);
+        //NPCと連動
+    }
+
+    public void AddStockTask()
+    {
+        GameManager.instance.taskManager.AddTask(Task.stocking);
+    }
+
+    public void AddDisplayTask()
+    {
+        GameManager.instance.taskManager.AddTask(Task.display);
+    }
+
+    public void AddCleanTask()
+    {
+        GameManager.instance.taskManager.AddTask(Task.cleaning);
+    }
+
+    public void StopRandomSpawn()
+    {
+        GameManager.instance.spawnTask = false;
+    }
+}
