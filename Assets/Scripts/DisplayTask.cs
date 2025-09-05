f525f4ada80008b53e65ed5cb467b6c1c7ce51de
5F\ktc <>
2025-09-05 18:07:32 +0900

    修正

--- Assets/Scripts/DisplayTask.cs
+++ Assets/Scripts/DisplayTask.cs
@@ -4,7 +4,11 @@ using UnityEngine.UI;
 public class DisplayTask : MonoBehaviour
 {
     [SerializeField] private GameObject[] poteti;
-    [SerializeField] private GameObject[] potetiefect;
+    //[SerializeField] private GameObject[] potetiefect;
+
+    public Transform[] target;
+    private LineRenderer line;
+
     private int taskcount = 0;
 
     [SerializeField] private Display task;
@@ -18,10 +22,18 @@ public class DisplayTask : MonoBehaviour
         {
             gobj.SetActive(false);
         }
-        foreach (var pe in potetiefect)
-        {
-            pe.SetActive(false);
-        }
+        //foreach (var pe in potetiefect)
+        //{
+        //    pe.SetActive(false);
+        //}
+
+        line = GetComponent<LineRenderer>();
+        line.positionCount = 5;
+        line.loop = false;
+        line.useWorldSpace = true;
+        line.widthMultiplier = 0.05f;
+
+        UpdateOutline();
     }
    
 
@@ -32,11 +44,25 @@ public class DisplayTask : MonoBehaviour
         if (task.crear)
         {
             poteti[taskcount].SetActive(true);
-            potetiefect[taskcount].SetActive(true);
+            //potetiefect[taskcount].SetActive(true);
             taskcount++;
             task.crear = false;
             taskcamera.OnCameraInvoke();
         }
         
+    }
+
+    void UpdateOutline()
+    {
+        Bounds bounds = target[taskcount].GetComponent<Renderer>().bounds;
+
+        Vector3[] points = new Vector3[5];
+        points[0] = new Vector3(bounds.min.x, bounds.min.y, bounds.min.z);
+        points[1] = new Vector3(bounds.max.x, bounds.min.y, bounds.min.z);
+        points[2] = new Vector3(bounds.max.x, bounds.max.y, bounds.min.z);
+        points[3] = new Vector3(bounds.min.x, bounds.max.y, bounds.min.z);
+        points[4] = points[0];
+
+        line.SetPositions(points);
     }
 }
